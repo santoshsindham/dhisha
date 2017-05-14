cc0c67012df40dda52cd2331e64abdcab7fe4667
Santosh Sindham <santosh.sindham@gmail.com>
2017-05-14 13:08:25 +0530

    included the rolify gem and added rails admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
+  rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  after_create :assign_default_role
+
+  def assign_default_role
+    self.add_role(:newuser) if self.roles.blank?
+  end
 end
