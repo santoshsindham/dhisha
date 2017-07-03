e68827375b5defbee062920f19e6cc9d9fa65ce7
Santosh Sindham <santosh.sindham@gmail.com>
2017-07-03 13:17:12 +0530

    integrating aws ses

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-         
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
   after_create :assign_default_role
 
   def assign_default_role
