84e2386b7472654e7f7280ce208737c7d53f075a
Santosh Sindham <santosh.sindham@gmail.com>
2017-07-18 17:10:59 +0530

    updated with the contact me form

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   get 'home/index'
   get 'home/vision'
   get 'home/objectives'
-  get 'home/about' 
+  get 'home/about'
   get 'home/contact'
-
+  get 'contact-me', to: 'messages#new', as: 'new_message'
+  post 'contact-me', to: 'messages#create', as: 'create_message'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
