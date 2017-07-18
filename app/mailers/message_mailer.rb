2f6ea957042327ec02057cf10a4f9fb51476afac
Santosh Sindham <santosh.sindham@gmail.com>
2017-07-18 17:54:13 +0530

    fixed the line break issue in emails

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -6,8 +6,8 @@ class MessageMailer < ApplicationMailer
   #   en.message_mailer.contact_me.subject
   #
   def contact_me(message)
-    @greeting = "Hi"
-    @body = "Hi," + "\n" + "Message from email id: "+ message.email + "\n" + message.body
+    @greeting = "Hello we have received a message at Dhisha.org"
+    @body = ("Dear Admin there is a message at Dhisha.org...!" + "<br /><br />\n" + "Message received from email id: "+ message.email + "<br /><br />\n Content received is as below:<br />\n" + message.body).html_safe
     mail to: "chandu3211@gmail.com", cc: "santosh.sindham@gmail.com", from: "contact@dhisha.org"
   end
 end
