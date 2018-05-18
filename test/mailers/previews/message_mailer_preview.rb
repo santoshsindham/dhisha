6367db69689e8c1c687407829cb811e5130803de
Santosh Sindham <santoshkumar.sindham@in.tesco.com>
2018-05-18 15:08:23 +0530

    Updated mailer preview

--- test/mailers/previews/message_mailer_preview.rb
+++ test/mailers/previews/message_mailer_preview.rb
@@ -3,7 +3,10 @@ class MessageMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/message_mailer/contact_me
   def contact_me
-    MessageMailer.contact_me
+    @message = Message.new
+    @message.email = 'test.testing@gmail.com'
+    @message.body = 'Thanks for contributing to Dhisha.'
+    MessageMailer.contact_me(@message)
   end
 
 end
