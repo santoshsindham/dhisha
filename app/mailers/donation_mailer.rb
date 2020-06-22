bb787045786bea18f058707090fca94b2953b873
Sindham, Santosh K <sindham_santosh@optum.com>
2020-06-22 20:51:49 +0530

    Testing squash function

--- app/mailers/donation_mailer.rb
+++ app/mailers/donation_mailer.rb
@@ -3,6 +3,6 @@ class DonationMailer < ApplicationMailer
   def thanks_email
     @user = params[:user]
     @donation = params[:donation]
-    mail(to: @user.email, subject: 'Thanks for making a donation @Dhisha.org')
+    mail(to: @user.email, subject: 'Thanks for making a donation to wwww.dhisha.org')
   end
 end
